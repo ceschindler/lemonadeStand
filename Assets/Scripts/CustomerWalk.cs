f58a549b598dfb778d019cd0645d745bd5246185
Collin Schindler <schindler.collin@gmail.com>
2024-05-28 21:44:41 -0500

    Build Customer 'Purchase' cycle. (#3)
    * Adding stopping locations and multiple customers
    
    * Added more customers
    
    Also fixed logic for timers making the customers wait the proper amount of time

--- Assets/Scripts/CustomerWalk.cs
+++ Assets/Scripts/CustomerWalk.cs
@@ -4,55 +4,83 @@ using UnityEngine;
 
 public class CustomerWalk : MonoBehaviour
 {
+    // Customer movement variables
     public float moveSpeed;
     private float originalSpeed;
+    private float xPosition;
     public float horizontalMovement;
     public float verticalMovement;
 
+    // Customer pause variables for getting lemonade
     private bool isPaused;
-    public float xPosition;
+    private float timer = 0.0f;
+    public float customerWaitTime;
+
+    // Customer stopping locations
+    private float[] locations;
+    private float stoppingLocation;
+
+
     // Start is called before the first frame update
     void Start()
     {
         isPaused = false;
         originalSpeed = moveSpeed;
+        locations = FindPossibleStoppingPointsForCustomers();
+        stoppingLocation = ChooseStoppingLocation();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Grab the x position of the customer at the start of the frame
         xPosition = transform.position.x;
-        Debug.Log(xPosition);
-        if (Input.GetKey("space")) 
+
+        // Determine if customer is in a valid location to stop for some lemonade
+        if ((xPosition > stoppingLocation) && !isPaused) 
         {
-            Debug.Log("Space presssed");
+            // Stop customer movement and start wait timer
+            isPaused = true;
             moveSpeed = 0;
-            isPaused = true;
-        } else if (Input.GetKey("up"))
-        {
-            moveSpeed = originalSpeed;
-            isPaused = false;
-        }
-        
-        if (xPosition == 0 && !isPaused) 
-        {
-            isPaused = true;
-            for (int i = 0; i < 300; i++) 
-            {
-                moveSpeed = 0;
-                transform.Translate(new Vector3(0, 0, 0));
-                // Pausing Customer
-                if (i % 100 == 0) 
-                {
-                    Debug.Log(i);
-                }
-            }
-            isPaused = false;
-            transform.Translate(Time.deltaTime, 0, 0);
+            timer = Time.time;
         } 
         else 
         {
+            // If game time is now longer than wait time, and the customer is in a valid stop spot
+            // reset their movement speed back to their original speed.
+            if ((Time.time > (timer + customerWaitTime)) && isPaused) 
+            {
+                moveSpeed = originalSpeed;
+            }
+            
+            // Move Customer
             transform.Translate(new Vector3(horizontalMovement, verticalMovement, 0) * moveSpeed);
         }
+    }
+
+    // Array of possible stopping points for the customer
+    public float[] FindPossibleStoppingPointsForCustomers() 
+    {
+        // Create array to store all possible stopping locations
+        float[] locations = new float[4];
+
+        // Iterate over all objects tagged with "Customer" in the scene
+        // These are small boxes hidden inside the lemonade stand
+        int index = 0;
+        foreach (GameObject location in GameObject.FindGameObjectsWithTag("Customer"))
+        {
+            // Add x position of GameObject to array of possible locations
+            locations[index] = location.transform.position.x;
+            index++;
+        }
+        return locations;
+    }
+
+    // Have customer choose a random stopping location from the list of possible locations
+    public float ChooseStoppingLocation() 
+    {
+        // Random random = new Random();
+        int location = Random.Range(0, locations.Length);
+        return locations[location];
     }
 }
