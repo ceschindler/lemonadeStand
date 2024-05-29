561300e9c521855fbc29f3af8f73d98696fb0c88
mploederer <ploederermacade@gmail.com>
2024-05-28 21:38:41 -0500

    Ingredient Menu w/ Submit and Confirm Screen
    Ingredient Menu  for Lemons Water and Sugar with changing Sprite Bar. Also added a submit Button that stores values for Total Lemons, Total Sugar and Total Water with a confirm panel that appears over the menu asking to start the game.

---
+++ Assets/Scripts/IngredientMenuScripts/ConfirmationWindow.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ConfirmationWindow2 : MonoBehaviour
+{
+    public Button yesButton;
+    public Button noButton;
+    public TextMeshProUGUI messageText;
+ 
+}
